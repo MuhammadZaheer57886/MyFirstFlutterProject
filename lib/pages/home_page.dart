ec5bb71366a63e577d7f16fcf8e7843823e9f7ca
MuhammadZaheer57886 <mza57886@gmail.com>
2022-09-03 14:56:47 +0500

    day 11 compleated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/models/catalog.dart';
+
+import 'package:my_first_app/widget/item_widget.dart';
 
 import '../widget/drawer.dart';
 
@@ -6,25 +9,23 @@ class HomePage extends StatelessWidget {
   final int hours = 8;
   final String foryou = "Zaheer bro this is";
 
-  const HomePage({Key? key}) : super(key: key);
-
+ HomePage({Key? key}) : super(key: key);
+  final dummylist = List.generate(5, (index) => CatalogModel.items[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Fareed App'),
       ),
-      body: Center(
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: Text(
-            '$foryou $hours hours cource of flutter',
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.teal,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummylist[index],
+            );
+          },
         ),
       ),
       drawer: const MyDrawer(),
