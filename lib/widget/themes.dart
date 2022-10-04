e23d74ff5e5cc4b90a7a4f70356afb69e90877a7
MuhammadZaheer57886 <mza57886@gmail.com>
2022-10-04 11:10:25 +0500

    day 14 completed

--- lib/widget/themes.dart
+++ lib/widget/themes.dart
@@ -4,12 +4,12 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
-      appBarTheme:  const AppBarTheme(
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      appBarTheme: const AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
         iconTheme: IconThemeData(
-        color: Colors.black,
+          color: Colors.black,
         ),
         titleTextStyle: TextStyle(color: Colors.black),
       ));
@@ -17,4 +17,8 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+  //Colors
+  static Color creamColor = const Color(0xfff5f5f5);  
+  static Color darkBluishColor = const Color(0xff403b58);  
 }
